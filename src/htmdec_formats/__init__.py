c61f08ada08f317cb79f8c895152eb0810fc31ba
Matthew Turk <matthewturk@gmail.com>
2024-04-26 11:05:05 -0500

    Renaming to a better name

--- src/htmdec_formats/__init__.py
+++ src/htmdec_formats/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Matthew Turk"""
 __email__ = "matthewturk@gmail.com"
 __version__ = "0.1.0"
 
-from .htmdec_formats import IndenterDataset
+from .indenter_formats import IndenterDataset
